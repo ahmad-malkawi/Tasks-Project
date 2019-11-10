f138bbc7e8d18368471ebe60aa1703fd1c8731dd
ahmad-malkawi <57582973+ahmad-malkawi@users.noreply.github.com>
2019-11-10 09:22:11 +0200

    amer

---
+++ tasks/src/jo/secondstep/session2/Subject.java
@@ -0,0 +1,22 @@
+package jo.secondstep.session2;
+
+public class Subject {
+	String name;
+	double mark;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getMark() {
+		return mark;
+	}
+
+	public void setMark(double subject_mark) {
+		this.mark = subject_mark;
+	}
+}
